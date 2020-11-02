2b269ac4d4ec91de8c14cfa536f83205587c92e9
J.Cruz <jcruzupr@users.noreply.github.com>
2020-11-02 16:23:42 -0400

    Add project files.

---
+++ CSV Reader/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CSV_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBoxAverages = new System.Windows.Forms.ListBox();
+            this.BtnProcessGrades = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBoxAverages
+            // 
+            this.LBoxAverages.FormattingEnabled = true;
+            this.LBoxAverages.Location = new System.Drawing.Point(12, 15);
+            this.LBoxAverages.Name = "LBoxAverages";
+            this.LBoxAverages.Size = new System.Drawing.Size(260, 134);
+            this.LBoxAverages.TabIndex = 0;
+            // 
+            // BtnProcessGrades
+            // 
+            this.BtnProcessGrades.Location = new System.Drawing.Point(28, 155);
+            this.BtnProcessGrades.Name = "BtnProcessGrades";
+            this.BtnProcessGrades.Size = new System.Drawing.Size(147, 23);
+            this.BtnProcessGrades.TabIndex = 1;
+            this.BtnProcessGrades.Text = "Get and Process Grades";
+            this.BtnProcessGrades.UseVisualStyleBackColor = true;
+            this.BtnProcessGrades.Click += new System.EventHandler(this.BtnProcessGrades_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(181, 155);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(75, 23);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 192);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnProcessGrades);
+            this.Controls.Add(this.LBoxAverages);
+            this.Name = "Form1";
+            this.Text = "CSV Reader Example";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LBoxAverages;
+        private System.Windows.Forms.Button BtnProcessGrades;
+        private System.Windows.Forms.Button BtnExit;
+    }
+}
+
---
+++ CSV Reader/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CSV_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnProcessGrades_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader myInputFile;
+                string rowOfGrades;
+                int total = 0 ;
+                double avg;
+                char delimiter = ',';  // assumes a comma delimted (CSV) file
+
+                // Open File (CSV = Comma Seperated Values)
+                myInputFile = File.OpenText("C:\\Users\\Jose\\Desktop\\App_Data\\Grades.csv");
+                // Edit the above path according to your environment.
+                // Note: Rememeber, if no path is indicated the files are saved in bin/debug/netcoreapp3.1
+
+                // Read file line by line
+                while (!myInputFile.EndOfStream)
+                {
+                    rowOfGrades = myInputFile.ReadLine();
+                    // Saves grades into a string array (get tokens)
+                    string[] individualGrades = rowOfGrades.Split(delimiter);
+
+                    total = 0;
+                    // Accumulate/Totalize the sum of all the grades
+                    foreach (string g in individualGrades)
+                    {
+                        total += Convert.ToInt32(g);
+                    }
+
+                    avg = (double)total / individualGrades.Length;
+
+                    // Add info of the row processed to ListBox
+                    LBoxAverages.Items.Add($"Grades: {rowOfGrades} ==> Avg. is {avg:n1}");
+                    
+                }
+
+                // Don't forget to close file when done
+                myInputFile.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
